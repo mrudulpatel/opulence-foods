0be5bcdda0fe993928c15fd28598f2af1237e23f
Mrudul Patel <mrudulpatel04@gmail.com>
2024-05-02 19:40:03 +0530

    Fix typo in Hero component's text content

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
           <span className="text-primary">OpulenceFoods</span>
         </h1>
         <p className="my-4 text-gray-500 text-lg">
-          "Snack Your Way to Bliss: Completing Everyday Joys!"
+          &dbquo;Snack Your Way to Bliss: Completing Everyday Joys!&dbquo;
         </p>
         <div className="flex gap-4">
           <button className="bg-primary text-sm items-center flex gap-2 text-white px-8 py-2 rounded-full">
