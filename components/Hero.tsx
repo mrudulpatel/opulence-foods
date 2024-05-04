c1c68b7be526484c1e7ebc0598876beeafd03b87
Mrudul Patel <mrudulpatel04@gmail.com>
2024-05-04 12:42:44 +0530

    chore: Update Hero component grid layout for larger screens

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="grid grid-cols-2 mb-8">
+    <section className="grid lg:grid-cols-2 mb-8">
       <div className="py-24">
         <h1 className="text-4xl font-semibold">
           Everything is better with&nbsp;
@@ -13,16 +13,6 @@ const Hero = () => {
         <p className="my-4 text-gray-500 text-lg">
           &#x301D;Snack Your Way to Bliss: Completing Everyday Joys!&#x301E;
         </p>
-        <div className="flex gap-4">
-          <button className="bg-primary text-sm items-center flex gap-2 text-white px-8 py-2 rounded-full">
-            Order Now
-            <ArrowRightCircleIcon className="h-6 w-6" />
-          </button>
-          <button className="flex gap-2 py-2 text-gray-600 font-semibold">
-            Learn More
-            <ArrowRightCircleIcon className="h-6 w-6" />
-          </button>
-        </div>
       </div>
       <div className="relative size-full">
         <Image
